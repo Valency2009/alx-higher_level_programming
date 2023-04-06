019059e07e8878ff4653a43d328de21f836088b5
Valency2009 <fakiyesivincent@gmail.com>
2023-04-06 15:14:13 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" definition of a square size"""
+
+
+class Square:
+    """
+    Attributes:
+    size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.size = size
