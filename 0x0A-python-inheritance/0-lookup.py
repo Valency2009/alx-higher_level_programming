d61e9bce25b40b6b21b3ab4533cce6356865796b
Valency2009 <fakiyesivincent@gmail.com>
2023-04-12 14:15:55 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """
-contains the MyList class
+Contains the lookup function
 """
 
 
-class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
-    def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
